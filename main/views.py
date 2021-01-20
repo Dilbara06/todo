08ccdee6e98076012d67d8ff800ce52f8d50a039
Dilbara06 <dkirbasheva@gmail.com>
2021-01-21 01:57:41 +0600

    added books info upload function

--- main/views.py
+++ main/views.py
@@ -24,3 +24,16 @@ def add_todo(request):
     todo.save()
     return redirect(test)
 
+def add_books (request):
+    form=request.POST
+    title=form["books_title"]
+    subtitle=form["books_subtitle"]
+    description=form["books_description"]
+    price=form["books_price"]
+    genre=form["books_genre"]
+    author=form["books_author"]
+    year=form["books_year"]
+    books=Books(title=title,subtitle=subtitle,description=description,price=price,genre=genre,author=author,year=year)
+    books.save()
+    return redirect(second)
+
